b50b6440614952af82ad83e90a4a1b639a62bb80
Andreas Haupt <indraos@freenet.de>
2019-04-01 22:52:39 +0200

    Added support for holidays

--- assets/code/davinci_to_lanis.py
+++ assets/code/davinci_to_lanis.py
@@ -7,45 +7,79 @@ import numpy # Für Vektorisierung
 # definitions
 weekdaytoind = {'Montag': 1, 'Dienstag': 2, 'Mittwoch': 3, 
                 'Donnerstag': 4, 'Freitag': 5, 'Samstag': 6, 
-                'Sonntag': 7}
+                'Sonntag': 7} # Lanis Online-Kodierung der Wochentage
 begintimetolesson = {t.time(7,30): 1, t.time(8,15): 2, t.time(9, 15): 3,
                      t.time(10,0): 4, t.time(11,0): 5, t.time(11,45): 6,
                      t.time(13,0): 7, t.time(13,45): 8, t.time(14,45): 9,
-                     t.time(15,30): 10,t.time(16,30): 11,t.time(17,15): 12}
-endtimetolesson = {t.time(8,15): 1, t.time(9,0): 2, t.time(10,0): 3, t.time(10,45): 4,
-                   t.time(11,45): 5, t.time(12,30): 6,t.time(13,45): 7, t.time(14,30): 8,
-                   t.time(15,30): 9,t.time(16,15): 10,t.time(17,15): 11,t.time(18,0): 12}
+                     t.time(15,30): 10,t.time(16,30): 11,t.time(17,15): 12} # Stunden-Beginnzeiten zu Stunden
+endtimetolesson = {t.time(8,15): 1, t.time(9,0): 2, t.time(10,0): 3, 
+                   t.time(10,45): 4, t.time(11,45): 5, t.time(12,30): 6,
+                   t.time(13,45): 7, t.time(14,30): 8, t.time(15,30): 9,
+                   t.time(16,15): 10,t.time(17,15): 11,t.time(18,0): 12} # Stunden-Endzeiten zu Stunden
 timestolesson = numpy.vectorize(lambda x,y: 
                                 str(begintimetolesson[x]) + '-' + str(endtimetolesson[y]) 
                                 if begintimetolesson[x] != endtimetolesson[y] 
-                                else str(begintimetolesson[x]))
+                                else str(begintimetolesson[x])) # Funktion, die Einzelstunden und Doppelstunden 
+                                                                # mit '-' oder als Einzelstunde passend darstellt.
+ab = lambda x: 'A' if x%2 == 1 else 'B' # Kodierung der Wochen
+
+def weeklists(x):
+    """
+    weeklists
+    input: Formatierter String
+    output: Liste
+    Bringe formatierte Liste mit Bindestrichen in Liste
+    """
+    if str(x)[0] != '(': # 'Jede Woche'-Eintrag wird als Regelunterricht betrachtet
+        return list(range(53))
+    parseranges = lambda x: list(range(int(x.split('-')[0]),int(x.split('-')[1])+1)) if '-' in x else [int(x)]
+    return sum(list(map(parseranges, x[1:-1].split(','))),[]) # Kriege eine Liste mit allen Wochen, 
+                                                              # in denen Unterricht stattfindet
 
 def weekstoab(x):
     '''
     weekstoab:
-    input: Formatierter String
+    input: Liste
     output: '', 'A', 'B' oder numpy.nan
-    Falls diese und nächte Woche Unterricht ist, zeige an, als sei immer Unterricht.
-    Falls in dieser Woche oder nächste Woche Unterricht ist und dort eine Abfolge von Unterricht-kein Unterricht-Unterricht
-    beginnt, so schreibe A, falls die Kalenderwoche, in der Unterricht it, ungerade ist, und B, fals sie gerade ist.
-    Sonst zeige an, es sei nie Unterricht.
-    '''
-    if not str(x)[0]=='(': # 'Jede Woche' mapped to empty Wochen entry
-        return ''
-    parseranges = lambda x: list(range(int(x.split('-')[0]),int(x.split('-')[1])+1)) if '-' in x else [int(x)]
-    x = sum(list(map(parseranges, x[1:-1].split(','))),[])
-    kw = t.date.today().isocalendar()[1] # get calendar week
-    if not (kw in x or kw+1 in x): # if none of the next two weeks is a lesson, drop the lesson
-        return numpy.nan
-    elif kw in x and kw+1 in x: # if both of the next two weeks is lesson assume lesson is every week
+    Nähert Unterricht in beliebigen Wochenzuteilungen durch A- und B-Wochen sowie Regelunterricht an.
+    '''   
+    kw = t.date.today().isocalendar()[1] # Kriege Kalenderwoche
+    kw = min(y for y in range(54) if y not in holidays and y >= kw) # wird der Stundenplan in den Ferien aktualisiert, behandeln wir die Anfrage, als sei sie in der ersten Woche nach den Ferien gestellt.
+    if kw+1 in holidays:   # in der letzten Schulwoche werden alle stattfindenden Stunden 
+                           # als wöchentlich betrachtet, alle anderen als inexistent
+        if kw in x:
             return ''
-    elif min( (y for y in x if y in x and not y+1 in x and y+2 in x and y >= kw), default=numpy.nan) <= kw+1:
-        return 'A' if min(y for y in x if y in x and not y+1 in x and y+2 in x and y >= kw)%2 else 'B'
+        else:
+            return numpy.nan
+    elif kw+2 in holidays:   
+        if kw in x and kw+1 in x:
+            return ''
+        elif kw in x and kw+1 not in x:
+            return ab(kw)
+        elif kw not in x and kw+1 in x:
+            return ab(kw+1)
+        else:
+            return numpy.nan
+                           # in der vorletzten Schulwoche werden alle Stunden, die in beiden Wochen stattfinden als
+                           # Regelunterricht betrachtet, Unterricht, der in genau einer Woche stattfindet als A-/B-Wochen-Unterricht
+                           # und Unterricht, der in beiden Wochen nicht stattfindet als inexistent.
     else:
-        return numpy.nan
+        if kw in x and  kw+1 not in x and kw+2 in x:
+            return ab(kw)
+        elif kw not in x and kw+1 in x and kw+2 not in x:
+            return ab(kw+1)
+        elif kw in x and kw+1 in x:
+            return ''
+        else:
+            return numpy.nan
+                           # alle anderen Wochen betrachten wir als ausreichend weit vor den Ferien. Diese werden als
+                           # A-/B-Woche betrachtet, falls entweder der Unterricht in den nächsten drei Wochen wie
+                           # Unterricht-kein Unterricht-Unterricht bzw. kein Unterricht-Unterricht-kein Unterricht ist.
+                           # Falls die nächsten beiden Wochen Unterricht ist, so wird der Unterricht als Regelunterricht
+                           # angenommen, sonst als nicht existent.
     
 # load data
-with open(sys.argv[1],'rb') as infile:
+with open('import.xls','rb') as infile:
     df = pd.read_excel(infile)
     
 # manipulation
@@ -57,7 +91,11 @@ df.dropna(subset=['Raum'], inplace=True) # kein Unterricht ohne Raum
 df = df.loc[df['Wochen'] != 'Einmalig'] # keine einmaligen Veranstaltungen
 df['Lehrerkuerzel'] = df['Lehrer']
 df['Kurs'] = df['Klasse']
-df['Woche'] = df['Wochen'].apply(weekstoab)
+
+holidays = list(set(range(53)).difference(set(sum(df['Wochen'][df['Wochen']!='Jede Woche'].apply(weeklists),[]))))
+df['Woche'] = df['Wochen'].apply(weeklists)
+df['Woche'] = df['Woche'].apply(weekstoab)
+
 df.dropna(subset=['Woche'], inplace=True) # Unterrichtseinheiten, die derzeit nicht stattfinden entfernen
 df['Klassen'] = df['Klasse']
 df['Id'] = range(0,df.index.size) # create index
